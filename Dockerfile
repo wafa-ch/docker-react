d9da23c68a983bae914f4f3b9a37b71f9f4157e1
wafa chenni <chenniwafa@gmail.com>
2020-04-19 11:20:15 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:alpine as builder
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
+# no CMD to start nginx because the startup command of nginx does it for us
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WAFA BISBIS   <code>WAFA --------------TIA tia --------------  BISBIS</code>   WAFA BISBIS .
         </p>
         <a
           className="App-link"
